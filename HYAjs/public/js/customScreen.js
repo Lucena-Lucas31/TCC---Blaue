42f005f560ee6d14bceeba740b534f21e916ebde
MurakamiJuliana <murakamijuliana@hotmail.com>
2023-08-03 09:21:41 -0300

    CustomGame alterações

--- HYAjs/public/js/customScreen.js
+++ HYAjs/public/js/customScreen.js
@@ -1,4 +1,4 @@
-const inputFile = document.querySelector('.pictureInput');
+const inputFile = document.querySelector("#pictureInput");
 const pictureImage = document.querySelector('.pictureImage');
 const pictureImageText = "Escolha uma Imagem";
 pictureImage.innerHTML = pictureImageText;
@@ -28,7 +28,38 @@ inputFile.addEventListener("change", function(e){
    
 });
 
+// ===========================================================================================
 
+const inputFile1 = document.querySelector("#pictureInput1");
+const pictureImage1 = document.querySelector(".pictureImage1");
+const pictureImageTxt1 = "Escolha uma imagem";
+pictureImage1.innerHTML = pictureImageTxt1;
+
+inputFile1.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file1 = inputTarget.files[0];
+    
+    
+  
+    if (file1) {
+      const reader = new FileReader();
+  
+      reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+  
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("pictureImg1");
+  
+        pictureImage1.innerHTML = "";
+        pictureImage1.appendChild(img);
+      });
+  
+      reader.readAsDataURL(file1);
+    } else {
+      pictureImage1.innerHTML = pictureImageTxt1;
+    }
+  });
 
 
 
