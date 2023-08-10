c69938d404e65c94e0de199241aa623dda5c8045
MurakamiJuliana <murakamijuliana@hotmail.com>
2023-08-10 08:35:55 -0300

    Desenvolvimento da customScreen

--- HYAjs/public/js/customScreen.js
+++ HYAjs/public/js/customScreen.js
@@ -1,38 +1,41 @@
-const inputFile = document.querySelector("#pictureInput");
-const pictureImage = document.querySelector('.pictureImage');
-const pictureImageText = "Escolha uma Imagem";
-pictureImage.innerHTML = pictureImageText;
+const inputFile = document.querySelector("#picture__input");
 
-inputFile.addEventListener("change", function(e){
-    const inputTarget = e.target;
-    const file = inputTarget.files[0];
 
-    if(file){
-        const reader = new FileReader();
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "Selecionar<br>Imagem";
+pictureImage.innerHTML = pictureImageTxt;
 
-        reader.addEventListener('load', function(e){
-            const readerTarget = e.target;
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+  
+  
 
-            const img = document.createElement('img');
-            img.src = readerTarget.result;
-            img.classList.add('pictureImg');
+  if (file) {
+    const reader = new FileReader();
 
-            pictureImage.innerHTML = "";
-            pictureImage.appendChild(img);
-        });
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
 
-        reader.readAsDataURL(file);
-    } else {
-        pictureImage.innerHTML = pictureImageText;
-    }
-   
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
 });
 
-// ===========================================================================================
+// ================================================================================
 
-const inputFile1 = document.querySelector("#pictureInput1");
-const pictureImage1 = document.querySelector(".pictureImage1");
-const pictureImageTxt1 = "Escolha uma imagem";
+const inputFile1 = document.querySelector("#picture__input1");
+const pictureImage1 = document.querySelector(".picture__image1");
+const pictureImageTxt1 = "Selecionar<br>Imagem";
 pictureImage1.innerHTML = pictureImageTxt1;
 
 inputFile1.addEventListener("change", function (e) {
@@ -49,7 +52,7 @@ inputFile1.addEventListener("change", function (e) {
   
         const img = document.createElement("img");
         img.src = readerTarget.result;
-        img.classList.add("pictureImg1");
+        img.classList.add("picture__img1");
   
         pictureImage1.innerHTML = "";
         pictureImage1.appendChild(img);
@@ -61,6 +64,133 @@ inputFile1.addEventListener("change", function (e) {
     }
   });
 
+  // ================================================================================
 
+const inputFile2 = document.querySelector("#picture__input2");
+const pictureImage2 = document.querySelector(".picture__image2");
+const pictureImageTxt2 = "Selecionar<br>Imagem";
+pictureImage2.innerHTML = pictureImageTxt2;
 
+inputFile2.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file2 = inputTarget.files[0];
+     
+  
+    if (file2) {
+      const reader = new FileReader();
+  
+      reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+  
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("picture__img2");
+  
+        pictureImage2.innerHTML = "";
+        pictureImage2.appendChild(img);
+      });
+  
+      reader.readAsDataURL(file2);
+    } else {
+      pictureImage2.innerHTML = pictureImageTxt2;
+    }
+  });
 
+// ================================================================================
+
+const inputFile3 = document.querySelector("#picture__input3");
+const pictureImage3 = document.querySelector(".picture__image3");
+const pictureImageTxt3 = "Selecionar<br>Imagem";
+pictureImage3.innerHTML = pictureImageTxt3;
+
+inputFile3.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file3 = inputTarget.files[0];
+    
+    
+  
+    if (file3) {
+      const reader = new FileReader();
+  
+      reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+  
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("picture__img3");
+  
+        pictureImage3.innerHTML = "";
+        pictureImage3.appendChild(img);
+      });
+  
+      reader.readAsDataURL(file3);
+    } else {
+      pictureImage3.innerHTML = pictureImageTxt3;
+    }
+  });
+
+// ================================================================================
+
+const inputFile4 = document.querySelector("#picture__input4");
+const pictureImage4 = document.querySelector(".picture__image4");
+const pictureImageTxt4 = "Selecionar<br>Imagem";
+pictureImage4.innerHTML = pictureImageTxt4;
+
+inputFile4.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file4 = inputTarget.files[0];
+    
+    
+  
+    if (file4) {
+      const reader = new FileReader();
+  
+      reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+  
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("picture__img4");
+  
+        pictureImage4.innerHTML = "";
+        pictureImage4.appendChild(img);
+      });
+  
+      reader.readAsDataURL(file4);
+    } else {
+      pictureImage4.innerHTML = pictureImageTxt4;
+    }
+  });
+
+// ================================================================================
+
+const inputFile5 = document.querySelector("#picture__input5");
+const pictureImage5 = document.querySelector(".picture__image5");
+const pictureImageTxt5 = "Selecionar<br>Imagem";
+pictureImage5.innerHTML = pictureImageTxt5;
+
+inputFile5.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file5 = inputTarget.files[0];
+    
+    
+  
+    if (file5) {
+      const reader = new FileReader();
+  
+      reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+  
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("picture__img5");
+  
+        pictureImage5.innerHTML = "";
+        pictureImage5.appendChild(img);
+      });
+  
+      reader.readAsDataURL(file5);
+    } else {
+      pictureImage5.innerHTML = pictureImageTxt5;
+    }
+  });
