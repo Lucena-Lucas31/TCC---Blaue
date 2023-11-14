b8174d20aa60d234ceaff66759b4e3445d0e7021
Leonardo Scarparo de Miranda <leoscmiranda@gmail.com>
2023-11-14 15:49:55 -0300

    Parte essencial do site pronta e estável

--- SITE TECHNO BLAUE/index.js
+++ SITE TECHNO BLAUE/index.js
@@ -58,7 +58,21 @@ document.getElementById("stage").addEventListener("change", function() {
       select.style.color = 'black'
     }
 
-    
-
-
 });
+
+const maxChecks = 1
+let selectedCount = 0
+
+document.querySelector('div').addEventListener('click', event => {
+  if (event.target.type === 'checkbox') {
+    selectedCount = event.target.checked
+      ? selectedCount + 1
+      : selectedCount - 1
+  }
+
+  if (selectedCount >= maxChecks) {
+    [...document.querySelectorAll('input:not(:checked)')].forEach(input => input.disabled = true)
+  } else {
+    [...document.querySelectorAll('input')].forEach(input => input.disabled = false)
+  }
+})
